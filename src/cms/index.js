ce862c6c757fe0f6d9a0192370e25feba1144f44
Q.Tran <tranlehaiquan@gmail.com>
2020-11-27 18:53:17 +0700

    registerPreviewTemplate notes

--- src/cms/index.js
+++ src/cms/index.js
@@ -2,6 +2,8 @@
 import CMS from 'netlify-cms-app';
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
+import NotePreview from './preview-templates/note-preview';
 
 CMS.registerPreviewTemplate('pages', PagePreview);
 CMS.registerPreviewTemplate('posts', PostPreview);
+CMS.registerPreviewTemplate('notes', NotePreview);
