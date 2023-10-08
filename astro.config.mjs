97747a4060d67c80d8e0abfbe0329b3e5b7547c7
Quan Tran <tranlehaiquan@gmail.com>
2023-10-08 14:27:57 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: "https://tranlehaiquan.github.io",
+  base: '/code-phe-blog',
   integrations: [
     tailwind({
       applyBaseStyles: false,
